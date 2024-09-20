2f8a109bd46aeb8bb3fecd338f0f9eaabd129ff3
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-09-20 11:00:40 +0200

    refactor: switch to axum

--- src/routes.rs
+++ src/routes.rs
@@ -3,74 +3,92 @@
 // see LICENSE for license details.
 
 use crate::db::{Database, Election, Slashed, Submission};
-use actix_web::{web, web::Json, Error, Result};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    Json,
+};
 use oasgen::{oasgen, OaSchema};
 use std::num::NonZeroUsize;
 
-type BoxError = Box<dyn std::error::Error>;
+type HttpError = (StatusCode, String);
 
 #[oasgen]
-pub async fn all_submissions(db: web::Data<Database>) -> Result<Json<Vec<Submission>>, Error> {
-    let submissions = db.get_all_submissions().await.map_err(BoxError::from)?;
+pub async fn all_submissions(
+    State(db): State<Database>,
+) -> Result<Json<Vec<Submission>>, HttpError> {
+    let submissions = db.get_all_submissions().await.map_err(internal_error)?;
     Ok(Json(submissions))
 }
 
 #[oasgen]
-pub async fn all_elections(db: web::Data<Database>) -> Result<Json<Vec<Election>>, Error> {
-    let winners = db.get_all_elections().await.map_err(BoxError::from)?;
+pub async fn all_elections(State(db): State<Database>) -> Result<Json<Vec<Election>>, HttpError> {
+    let winners = db.get_all_elections().await.map_err(internal_error)?;
     Ok(Json(winners))
 }
 
 #[oasgen]
-pub async fn all_slashed(db: web::Data<Database>) -> Result<Json<Vec<Slashed>>, Error> {
-    let slashed = db.get_all_slashed().await.map_err(BoxError::from)?;
+pub async fn all_slashed(State(db): State<Database>) -> Result<Json<Vec<Slashed>>, HttpError> {
+    let slashed = db.get_all_slashed().await.map_err(internal_error)?;
     Ok(Json(slashed))
 }
 
 #[oasgen]
 pub async fn most_recent_submissions(
-    db: web::Data<Database>,
-    info: web::Path<usize>,
-) -> Result<Json<Vec<Submission>>, Error> {
-    let n = into_non_zero_usize(info.into_inner())?;
+    State(db): State<Database>,
+    Path(n): Path<usize>,
+) -> Result<Json<Vec<Submission>>, HttpError> {
+    let n = into_non_zero_usize(n)?;
     let submissions = db
         .get_most_recent_submissions(n)
         .await
-        .map_err(BoxError::from)?;
+        .map_err(internal_error)?;
     Ok(Json(submissions))
 }
 
 #[oasgen]
 pub async fn most_recent_elections(
-    db: web::Data<Database>,
-    info: web::Path<usize>,
-) -> Result<Json<Vec<Election>>, Error> {
-    let n = into_non_zero_usize(info.into_inner())?;
+    State(db): State<Database>,
+    Path(n): Path<usize>,
+) -> Result<Json<Vec<Election>>, HttpError> {
+    let n = into_non_zero_usize(n)?;
     let winners = db
         .get_most_recent_elections(n)
         .await
-        .map_err(BoxError::from)?;
+        .map_err(internal_error)?;
     Ok(Json(winners))
 }
 
 #[oasgen]
 pub async fn most_recent_slashed(
-    db: web::Data<Database>,
-    info: web::Path<usize>,
-) -> Result<Json<Vec<Slashed>>, Error> {
-    let n = into_non_zero_usize(info.into_inner())?;
+    State(db): State<Database>,
+    Path(n): Path<usize>,
+) -> Result<Json<Vec<Slashed>>, HttpError> {
+    let n = into_non_zero_usize(n)?;
     let slashed = db
         .get_most_recent_slashed(n)
         .await
-        .map_err(BoxError::from)?;
+        .map_err(internal_error)?;
     Ok(Json(slashed))
 }
 
 // Convert a usize into a NonZeroUsize, returning an error if the value is zero.
 //
 // oasgen doesn't support NonZero types yet, so we have to do this manually.
-fn into_non_zero_usize(value: usize) -> Result<NonZeroUsize, Error> {
+fn into_non_zero_usize(value: usize) -> Result<NonZeroUsize, HttpError> {
     NonZeroUsize::new(value).ok_or_else(|| {
-        actix_web::error::ErrorBadRequest("/path/n must be positive integer".to_string())
+        (
+            StatusCode::BAD_REQUEST,
+            "path param value must be non-zero".to_string(),
+        )
     })
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> HttpError
+where
+    E: std::fmt::Display,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
