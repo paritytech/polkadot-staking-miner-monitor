bbf0878eccebac40c2ed965079f9f6d266b0349b
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-11-08 15:05:58 +0100

    add UNINITIALIZED=3 as default value

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -7,17 +7,23 @@ use metrics_exporter_prometheus::PrometheusBuilder;
 const TARGET: &str = "polkadot_election_status";
 const DESCRIPTION: &str = "The outcome of the most recent election represented as a integer. 0 if the election failed, 1 if the election succeeded based on unsigned solution and 2 if the election succeeded based on signed solution.";
 
+const FAILED: u32 = 0;
+const UNSIGNED: u32 = 1;
+const SIGNED: u32 = 2;
+const UNINITIALIZED: u32 = 3;
+
 pub fn setup_metrics_recorder() -> anyhow::Result<PrometheusHandle> {
     let handle = PrometheusBuilder::new().install_recorder()?;
     describe_gauge!(TARGET, DESCRIPTION);
+    metrics::gauge!(TARGET, "handle" => "handle").set(UNINITIALIZED);
     Ok(handle)
 }
 
 pub fn record_election(election_result: &ElectionResult) {
     let val = match election_result {
-        ElectionResult::Failed => 0,
-        ElectionResult::Unsigned => 1,
-        ElectionResult::Signed(_) => 2,
+        ElectionResult::Failed => FAILED,
+        ElectionResult::Unsigned => UNSIGNED,
+        ElectionResult::Signed(_) => SIGNED,
     };
     metrics::gauge!(TARGET, "handle" => "handle").set(val);
 }
