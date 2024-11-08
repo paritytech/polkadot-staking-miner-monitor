3d01b388d0246a1611d1e4a41e25c12126aa1d4a
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-11-08 16:44:40 +0100

    fix grumbles

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -1,29 +1,35 @@
+pub use election_status::record_election;
 pub use metrics_exporter_prometheus::PrometheusHandle;
 
-use crate::types::ElectionResult;
 use metrics::describe_gauge;
 use metrics_exporter_prometheus::PrometheusBuilder;
 
-const TARGET: &str = "polkadot_election_status";
-const DESCRIPTION: &str = "The outcome of the most recent election represented as a integer. 0 if the election failed, 1 if the election succeeded based on unsigned solution, 2 if the election succeeded based on signed solution or 3 if no election has occurred yet";
-
-const FAILED: u32 = 0;
-const UNSIGNED: u32 = 1;
-const SIGNED: u32 = 2;
-const WAITING_FOR_ELECTION: u32 = 3;
-
 pub fn setup_metrics_recorder() -> anyhow::Result<PrometheusHandle> {
     let handle = PrometheusBuilder::new().install_recorder()?;
-    describe_gauge!(TARGET, DESCRIPTION);
-    metrics::gauge!(TARGET, "handle" => "handle").set(WAITING_FOR_ELECTION);
+    describe_gauge!(election_status::TARGET, election_status::DESCRIPTION);
+    metrics::gauge!(election_status::TARGET)
+        .set(election_status::ElectionStatus::Unitialized as u32);
     Ok(handle)
 }
 
-pub fn record_election(election_result: &ElectionResult) {
-    let val = match election_result {
-        ElectionResult::Failed => FAILED,
-        ElectionResult::Unsigned => UNSIGNED,
-        ElectionResult::Signed(_) => SIGNED,
-    };
-    metrics::gauge!(TARGET, "handle" => "handle").set(val);
+pub(super) mod election_status {
+    use crate::types::ElectionResult;
+
+    pub(super) const TARGET: &str = "polkadot_election_status";
+    pub(super) const DESCRIPTION: &str = "The outcome of the most recent election represented as a integer. 0 if the election failed, 1 if the election succeeded based on unsigned solution, 2 if the election succeeded based on signed solution or 3 if no election has occurred yet";
+    pub(super) enum ElectionStatus {
+        Unitialized = 0,
+        Unsigned = 1,
+        Signed = 2,
+        Failed = 3,
+    }
+
+    pub fn record_election(election_result: &ElectionResult) {
+        let val = match election_result {
+            ElectionResult::Failed => ElectionStatus::Failed,
+            ElectionResult::Unsigned => ElectionStatus::Unsigned,
+            ElectionResult::Signed(_) => ElectionStatus::Signed,
+        };
+        metrics::gauge!(TARGET).set(val as u32);
+    }
 }
